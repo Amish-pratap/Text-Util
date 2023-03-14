c442f0fc8b81b1f7c985d98649a1baeab23f2273
Amish Pratap Singh <amishpratapsingh13@gmail.com>
2023-03-14 22:45:03 +0530

    added the darkmode toggel in the navebar

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,13 +1,24 @@
 
+import React,{ useState } from 'react';
 import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
 function App() {
+  const [mode,setMode]=useState('light');  //wheather dark mode is enable or not
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setMode('dark');
+    }
+    else{
+      setMode('light');
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" about='About TextUtils' />
+      <Navbar mode={mode} toggleMode={toggleMode} title="TextUtils" about='About TextUtils' />
       <div className='container my-3'>
         <TextForm heading='Enter the text to analyze below' />
         {/* <About/> */}
